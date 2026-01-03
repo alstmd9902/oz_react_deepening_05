75ab334abaf3369cb74e1098d8f4a9be82087145
Seungyeon Oh <alstmd9902@naver.com>
2026-01-03 18:10:00 +0900

    feat: 상품 추가 및 장바구니 합계 계산 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import Cart from './components/Cart';
 const App = () => {
     const [cart, setCart] = useState([]);
 
+    // TODO: 카트에 상품을 추가하는 함수를 완성하세요.
+    // 상품은 배열객체로 이루어져 있습니다.
+    // 카트에 상품이 8개 이상이면 alert를 띄워
+    // 더 이상 상품을 추가할 수 없다고 알려주세요.
     const addToCart = (product) => {
-        // TODO: 카트에 상품을 추가하는 함수를 완성하세요.
-        // 상품은 배열객체로 이루어져 있습니다.
-        // 카트에 상품이 8개 이상이면 alert를 띄워
-        // 더 이상 상품을 추가할 수 없다고 알려주세요.
+        if (cart.length >= 8) {
+            alert('8개 넘음');
+        }
+        setCart([...cart, product]);
     };
 
     return (
